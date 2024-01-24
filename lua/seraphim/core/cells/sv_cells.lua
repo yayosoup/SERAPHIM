5cad0315053594fe7f6a6b8eec8741807c72b113
mario <yayosoup@gmail.com>
2024-01-23 21:25:18 -0800

    the bug of doom as been tamed.

--- lua/seraphim/core/cells/sv_cells.lua
+++ lua/seraphim/core/cells/sv_cells.lua
@@ -3,10 +3,13 @@ local _P = FindMetaTable("Player")
 util.AddNetworkString("updateCells")
 
 function _P:CellsSave()
+    print("Saving cells for " .. self:Nick() .. self:GetNWInt("cells"))
     self:SetPData("cells", self:GetNWInt("cells"))
 end
 
 function _P:CellsLoad()
+    local testCells = self:GetPData("cells")
+    print("Loading cells for " .. self:Nick() .. testCells)
     if  self:GetPData("cells") == nil  then
         self:SetPData("cells", 1)
         self:SetNWInt("cells", 1)
@@ -19,7 +22,7 @@ function _P:CellsLoad()
 end
 
 function _P:AddCells(n)
-    self:SetNWInt("cells", self:GetNWInt("cells") + n)
+    self:SetNWInt("cells", self:GetPData("cells") + n)
     self:CellsSave()
 end
 
@@ -30,9 +33,8 @@ end)
 hook.Add("PlayerDisconnected", "CellsSave", function(ply)
     ply:CellsSave()
 end)
---[[
-timer.Create("CellsCheck", 5, 0, function()
 
+timer.Create("CellsCheck", 5, 0, function()
     for k , v in pairs(player.GetAll()) do
         print("Adding cells to " .. v:Nick())
         v:AddCells(1)
@@ -41,4 +43,3 @@ timer.Create("CellsCheck", 5, 0, function()
         net.Send(v)
     end
 end)
---]]
